38f2c17a51dd4413a2f00f9146a46cfed25744a3
Siang Jun Teo <siangjun1707@gmail.com>
2021-03-06 23:31:16 +0000

    fixed stuff...

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int curHealth = 0;
+    public int maxHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : Health
+{
+    public HealthBar healthBar;
+
+    
+    public void DamagePlayer( int damage )
+    {
+        curHealth -= damage;
+
+        healthBar.SetHealth( curHealth );
+    }
+}
