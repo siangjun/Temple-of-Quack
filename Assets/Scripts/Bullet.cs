f8636abf0b94f96bab707df0ff157e387e3c70c4
charrink <68334961+charrink@users.noreply.github.com>
2021-03-06 22:36:01 +0000

    shootygoose shoots

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rigidBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody.velocity = transform.right * speed;
+    }
+}
