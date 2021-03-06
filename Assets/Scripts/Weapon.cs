490eb39a78b5bb8f257d7d98085ed5fccef99cff
charrink <68334961+charrink@users.noreply.github.com>
2021-03-06 19:40:30 +0000

    shoot towards mouse

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,10 +7,19 @@ public class Weapon : MonoBehaviour
     //setup
     public Transform firePoint;
     public GameObject featherPrefab;
+    Vector2 mousePosition;
+    float lookAngle;
 
     // Update is called once per frame
     void Update()
     {
+        //rotating the firePoint to follow the mouse
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lookAngle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+
+
+        firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
+
         //checks if player is attacking
         if(Input.GetButton("Fire1")) 
         {
