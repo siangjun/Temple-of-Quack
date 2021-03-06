456121325e694fd21c1aaaeba1bac41b7fe0f052
charrink <68334961+charrink@users.noreply.github.com>
2021-03-06 16:58:14 +0000

    controller stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    //setup
     Rigidbody2D body;
     float horizontal;
     float vertical;
@@ -12,10 +13,15 @@ public class PlayerController : MonoBehaviour
 
     public float runSpeed = 10.0f;
 
+    //sprite setup
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] duckSprites;
+
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,6 +30,17 @@ public class PlayerController : MonoBehaviour
         //built-in support for arrow keys and wasd keys
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        //update sprite
+        if (horizontal < 0) {
+            spriteRenderer.sprite = duckSprites[1];
+            print("left");
+        } 
+        else if (horizontal > 0)
+        {
+            spriteRenderer.sprite = duckSprites[0];
+            print("right");
+        }
 
         //checks if player is attacking
         if(Input.GetKeyDown("space")) 
@@ -43,11 +60,16 @@ public class PlayerController : MonoBehaviour
         }
         //calculate new velocity of player
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        
     }
 
     void Attack() 
     {
         print("attack!");
+    }
+
+    void ChangeSprite(Sprite newSprite) {
+        spriteRenderer.sprite = newSprite;
     }
 
 }
