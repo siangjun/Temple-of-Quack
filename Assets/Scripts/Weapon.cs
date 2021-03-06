7c61552823346f4c67d9ea9b1975024f505becea
ShinobuDonuts <Shinobudonuts@outlook.com>
2021-03-06 21:47:06 +0000

    shooting centered

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,21 +7,24 @@ public class Weapon : MonoBehaviour
     //setup
     public Transform firePoint;
     public GameObject featherPrefab;
-    Vector2 mousePosition;
     float lookAngle;
 
     // Update is called once per frame
     void Update()
     {
         //rotating the firePoint to follow the mouse
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        lookAngle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        //mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        lookAngle = Mathf.Atan2(mousePosition.y + 2, mousePosition.x) * Mathf.Rad2Deg;
 
 
         firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
+        Debug.Log(lookAngle);
 
         //checks if player is attacking
-        if(Input.GetButton("Fire1")) 
+        if (Input.GetButton("Fire1")) 
         {
             Attack();
         }
