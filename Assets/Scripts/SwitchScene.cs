35fc69b24727b5d08ca381bc5069ee3ab16aa31a
Yaxin Ding <yding41@sheffield.ac.uk>
2021-03-06 20:07:34 +0000

    linked maps together finally :D

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Duck"))
+        {
+            Debug.Log("its Colliding!");
+            SceneManager.LoadScene(newLevel);
+        }
+    }
+    
+}
